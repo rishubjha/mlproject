e4d7b26f2faf4678dd1328aed4729bca44a8c689
Rishub Jha <rishubjha34@gmail.com>
2023-03-14 17:19:22 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -10,7 +10,6 @@ def error_message_detail(error,error_detail:sys):
     return error_message
 
     
-
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
@@ -20,10 +19,9 @@ class CustomException(Exception):
         return self.error_message
     
 # if __name__=="__main__":
-    
-# 	try:
+
+#     try:
 #         a=1/0
-#     except:
-# 		raise CustomException
-        
-#     logging.info("Logging has started")
+#     except Exception as e:
+#         logging.info("Divide by Zero")
+#         raise CustomException(e,sys)
